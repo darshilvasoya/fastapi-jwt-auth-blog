b48f6e2810a8261c9913de22d7a903a563e9bae5
darshilvasoya <darshilvy@gmail.com>
2025-08-14 03:10:51 +0530

    Add Render config for Python FastAPI deployment

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-
-# Upgrade pip (optional but recommended)
 pip install --upgrade pip
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Start the FastAPI app
 uvicorn main:app --host 0.0.0.0 --port $PORT
